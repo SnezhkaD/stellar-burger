3054b37d6c78f22a2d15509097e7f33295adcd50
Snezhana Dyachenko <Snezhka.1988@yandex.ru>
2024-07-18 11:33:23 +0300

    second

--- cypress/e2e/constructor.cy.tsx
+++ cypress/e2e/constructor.cy.tsx
@@ -8,7 +8,7 @@ beforeEach(() => {
   window.localStorage.setItem('refreshToken', 'testRefreshToken');
   cy.setCookie('accessToken', 'testAccessToken');
 
-   cy.fixture('ingredients.json').then((ingredients) => {
+  cy.fixture('ingredients.json').then((ingredients) => {
     cy.intercept(
       {
         method: 'GET',
@@ -54,7 +54,7 @@ describe('Проверка работы приложения', () => {
   const bunSelector = `[data-cy=bun_0]`;
   const ingredientSelector = `[data-cy=ingredient_0]`;
 
-  it('сервис доступен по адресу localhost:4000', () => { });
+  it('сервис доступен по адресу localhost:4000', () => {});
 
   it('добавление булок и ингредиентов', () => {
     cy.get(noBunSelector1).as('noBunText1');
@@ -63,7 +63,7 @@ describe('Проверка работы приложения', () => {
     cy.get(bunSelector + ` button`).as('bun');
     cy.get(ingredientSelector + ` button`).as('ingredient');
 
-   cy.get('@noBunText1').contains('Выберите булки');
+    cy.get('@noBunText1').contains('Выберите булки');
     cy.get('@noBunText2').contains('Выберите булки');
     cy.get('@noIngredientsText').contains('Выберите начинку');
 
@@ -83,13 +83,6 @@ describe('Проверка работы приложения', () => {
   });
 
   it('проверка нового заказа', () => {
-    const bun = cy.get(bunSelector + ` button`);
-    const ingredient = cy.get(ingredientSelector + ` button`);
-    bun.click();
-    ingredient.click({ multiple: true });
-
-    cy.get(`[data-cy=new_order_total] button`).click();
-
     cy.fixture('newOrder.json').then((newOrder) => {
       cy.intercept(
         {
@@ -99,16 +92,25 @@ describe('Проверка работы приложения', () => {
         newOrder
       ).as('newOrder');
 
-      cy.get(`[data-cy=new_order_number]`).contains(newOrder.order.number);
-      cy.get(`[data-cy=close_modal_btn]`).click();
+      const bun = cy.get(bunSelector + ` button`);
+      const ingredient = cy.get(ingredientSelector + ` button`);
+      bun.click();
+      ingredient.click({ multiple: true });
 
-      cy.get(noBunSelector1).as('noBunText1');
-      cy.get(noBunSelector2).as('noBunText2');
-      cy.get(noIngredientsSelector).as('noIngredientsText');
+      cy.get(`[data-cy=new_order_total] button`).click();
 
-      cy.get('@noBunText1').contains('Выберите булки');
-      cy.get('@noBunText2').contains('Выберите булки');
-      cy.get('@noIngredientsText').contains('Выберите начинку');
+      cy.wait('@newOrder').then(() => {
+        cy.get(`[data-cy=new_order_number]`).contains(newOrder.order.number);
+        cy.get(`[data-cy=close_modal_btn]`).click();
+
+        cy.get(noBunSelector1).as('noBunText1');
+        cy.get(noBunSelector2).as('noBunText2');
+        cy.get(noIngredientsSelector).as('noIngredientsText');
+
+        cy.get('@noBunText1').contains('Выберите булки');
+        cy.get('@noBunText2').contains('Выберите булки');
+        cy.get('@noIngredientsText').contains('Выберите начинку');
+      });
     });
   });
 });
